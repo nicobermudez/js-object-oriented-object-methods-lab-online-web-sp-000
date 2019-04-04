4e78afd58a14665a70c8c2dd6773ee3cc4074ae3
Nicolas Bermudez <bermnicolas@gmail.com>
2019-04-04 15:02:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,6 +16,6 @@ function BoardMember(name, homeState, training) {
     return "You will see great things from Scuber."
   }
   this.sayHi = function() {
-    "Hi my name is ${name}. I am from ${homestate}, and I was trained in ${training}."
+    return "Hi my name is ${name}. I am from ${homestate}, and I was trained in ${training}."
   }
 }
